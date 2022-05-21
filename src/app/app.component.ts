98c5f1f5d0eb5204a072a9b7cade5fed5421069c
sofia <sofiainesschenone@gmail.com>
2022-05-21 12:00:47 -0300

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { HolaService } from './services/hola.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers:[HolaService]
 })
 export class AppComponent {
   title = 'sofia-schenone';
+  constructor(private holaService: HolaService){}
+  ngOnInit(){
+    this.holaService.getData().subscribe(
+      data=>{
+        console.log('Res', data);
+      },
+      err=>{
+        console.warn(err);
+      }
+    )
+  }
+
 }
